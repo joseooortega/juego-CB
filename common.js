70423a5bca0ba69b9b64e48204945e4342e9df28
joseooortega <joseooortega@gmail.com>
2017-12-03 19:41:25 +0100

    Cambios JS

--- common.js
+++ common.js
@@ -1,13 +1,12 @@
 
+var keyState = {};
 
 $(document).ready(function(){
 
-  var keyState = {};
-  var personajes = [];
   var gameInterval = false;
   var checkCollision = false;
   window.container = $('#canvas');
-
+  var game = new game();
 
   window.addEventListener('keydown',function(e){
       keyState[e.keyCode || e.which] = true;
@@ -17,35 +16,102 @@ $(document).ready(function(){
       keyState[e.keyCode || e.which] = false;
   },true);
 
+  function test(objeto){
+    this.width = 100;
+  }
+  function moverX(x, objeto, velocidad){
+
+    velocidad = typeof velocidad === typeof undefined ? objeto.velocidad : velocidad;
+
+    if (x == 'left'){
+      if (objeto.positionX - velocidad > 0){
+        objeto.positionX -= velocidad;
+        objeto.element.css('left', objeto.positionX);
+      }else{
+        objeto.positionX = 0;
+        objeto.element.css('left', objeto.positionX);
+      }
+    }
+
+    if (x == 'right'){
+      if (window.container.width() > (objeto.positionX + velocidad + objeto.width)){
+        objeto.positionX += velocidad;
+        objeto.element.css('left', objeto.positionX);
+      }else{
+        objeto.positionX = window.container.width() - objeto.width;
+        objeto.element.css('left', objeto.positionX);
+      }
+    }
+
+    if (x == 'down'){
+      if ((objeto.positionY - velocidad) > 0){
+        objeto.positionY -= velocidad;
+        objeto.element.css('bottom', objeto.positionY);
+      }else{
+        objeto.positionY = 0;
+        objeto.element.css('bottom', objeto.positionY);
+      }
+    }
+
+    if (x == 'up'){
+      if ((objeto.positionY + velocidad + objeto.height) < window.container.height()){
+        objeto.positionY += velocidad;
+        objeto.element.css('bottom', objeto.positionY);
+      }else{
+        objeto.positionY = window.container.height() - objeto.height;
+        objeto.element.css('bottom', objeto.positionY);
+      }
+    }
+
+    return objeto;
+  }
+  function updatePositions(objeto){
+    objeto.element.css('left', objeto.positionX);
+    objeto.element.css('bottom', objeto.positionY);
+    return objeto;
+  }
+  function changeDimensions(objeto){
+    objeto.element.css('height', this.height);
+    objeto.element.css('width', this.width);
+    return objeto;
+  }
+
   function game(){
+
     this.intervalSeconds = 70;
     this.personaje = new personaje();
     this.personajes = [];
 
-    this.init = function(){
-      this.personajes.push(this.personaje);
-      this.gameLoop();
-      this.checkCollision();
-    }
-    this.checkCollision = function(){
+    test(this.personaje);
+    console.log(this.personaje);
 
+    this.init = function(){
+      this.generateEnemies();
+      var that = this;
+      setInterval(function(){ that.gameLoop(); }, 50);
     }
     this.gameLoop = function() {
 
       // Key events
-      if (keyState[37]) this.personaje.moverX('left');
-      if (keyState[39]) this.personaje.moverX('right');
-      if (keyState[38]) this.personaje.moverX('up');
-      if (keyState[40]) this.personaje.moverX('down');
-      if (keyState[32]) {}             //Spacebar
-
-      setTimeout(this.gameLoop, 50);
+      /*if (keyState[37]){ this = moverX('left', this.personaje);}
+      if (keyState[39]) this = moverX('right', this.personaje);
+      if (keyState[38]) this = moverX('up', this.personaje);
+      if (keyState[40]) this = moverX('down', this.personaje);
+      if (keyState[32]) {}             //Spacebar*/
     }
+    this.generateEnemies = function(){
+      var that = this;
+      setInterval(function(){
+        that.personajes.push(new enemigo_1());
+      }, 1000);
+    }
+    this.init();
   }
 
   function personaje(){
 
      this.vida = 100;
+     this.daño = 1;
      this.sprite = '';
      this.positionX = 0;
      this.positionY = 0;
@@ -59,69 +125,48 @@ $(document).ready(function(){
        var id = Math.floor((Math.random() * 9999) + 0);
        window.container.append('<div class="personaje" id="personaje_'+id+'"></div>');
        this.element = $('#personaje_'+id);
-       this.changeDimensions();
-     }
-     this.moverX = function(x, velocidad){
-
-       velocidad = typeof velocidad === typeof undefined ? this.velocidad : velocidad;
-
-       if (x == 'left'){
-         if (this.positionX - velocidad > 0){
-           this.positionX -= velocidad;
-           this.element.css('left', this.positionX);
-         }else{
-           this.positionX = 0;
-           this.element.css('left', this.positionX);
-         }
-       }
-
-       if (x == 'right'){
-         if (window.container.width() > (this.positionX + velocidad + this.width)){
-           this.positionX += velocidad;
-           this.element.css('left', this.positionX);
-         }else{
-           this.positionX = window.container.width() - this.width;
-           this.element.css('left', this.positionX);
-         }
-       }
-
-       if (x == 'down'){
-         if ((this.positionY - velocidad) > 0){
-           this.positionY -= velocidad;
-           this.element.css('bottom', this.positionY);
-         }else{
-           this.positionY = 0;
-           this.element.css('bottom', this.positionY);
-         }
-       }
-
-       if (x == 'up'){
-         if ((this.positionY + velocidad + this.height) < window.container.height()){
-           this.positionY += velocidad;
-           this.element.css('bottom', this.positionY);
-         }else{
-           this.positionY = window.container.height() - this.height;
-           this.element.css('bottom', this.positionY);
-         }
-       }
-
-     }
-     this.changeDimensions = function(){
-       this.element.css('height', this.height);
-       this.element.css('width', this.width);
-     }
-     this.updatePositions = function(){
-       this.element.css('left', this.positionX);
-       this.element.css('bottom', this.positionY);
+       this = changeDimensions(this);
      }
      this.init();
-
   }
+  function pPrincipal(){
+    personaje.apply(this,arguments);
+    this.disparos = 10;
+    this.proyectiles = [];
+    this.vida = 5;
+    this.daño = 100;
 
+    this.disparar = function(){
+      if (this.disparos > 0){
+        this.disparos--;
+        this.proyectiles.push(new proyectil());
+      }
+    }
+  }
+  function proyectil(){
+    this.width = 5;
+    this.height = 3;
+    this.velocidad = 11;
+    this.positionX = 0;
+    this.positionY = 0;
+    this.intervalSeconds = 70;
+    this.element = false;
 
-
-
-
+    this.init(){
+      var id = Math.floor((Math.random() * 9999) + 0);
+      window.container.append('<div class="personaje" id="personaje_'+id+'"></div>');
+      this.element = $('#personaje_'+id);
+      this = changeDimensions(this);
+      this.mover();
+    }
+    this.mover = function(){
+      var that = this;
+      setInterval(function(){
+        that = moverX('left', that, that.velocidad);
+        that = updatePositions(that);
+      }, that.intervalSeconds);
+    }
+  }
   function enemigo_1(){
     personaje.apply(this,arguments);
 
@@ -132,16 +177,16 @@ $(document).ready(function(){
     this.velocidad = 20;
 
     this.init2 = function(){
-      this.changeDimensions();
-      this.updatePositions();
+      this = changeDimensions(this);
+      this = updatePositions(this);
       this.mover();
     }
     this.mover = function(){
       var that = this;
       var i = setInterval(function(){
 
-        that.moverX('left', that.velocidad)
-        that.updatePositions();
+        moverX('left', that, that.velocidad);
+        that = updatePositions(that);
 
         if (that.positionX == 0){
           that.destroy();
